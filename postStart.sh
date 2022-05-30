0a0fbb70bf99af2b82841191c7570a52442c2c0f
truongdx271 <truongdinh271@gmail.com>
2022-05-30 17:07:16 +0700

    fix script

--- postStart.sh
+++ postStart.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-curl -X POST -H 'Content-type: application/json' --data "{"text":"POD: $HOSTNAME - IP: $MY_POD_IP - NS: $MY_POD_NAMESPACE has been started"}" https://hooks.slack.com/services/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
+curl -X POST "https://hooks.slack.com/services/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" \
+-H 'Content-type: application/json' \
+--data @<(cat <<EOF
+{
+  "text":"$HOSTNAME|$MY_POD_IP|$MY_POD_NAMESPACE"
+}
+EOF
+)
