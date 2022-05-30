1ac742693f15b0d025e8a0e3bc6b7da802bf9365
truongdx271 <truongdinh271@gmail.com>
2022-05-30 17:40:07 +0700

    add preStop.sh

--- preStop.sh
+++ preStop.sh
@@ -3,7 +3,7 @@ curl -X POST "$SLACK_URL" \
 -H 'Content-type: application/json' \
 --data @<(cat <<EOF
 {
-  "text":"$HOSTNAME|$MY_POD_IP|$MY_POD_NAMESPACE started"
+  "text":"$HOSTNAME|$MY_POD_IP|$MY_POD_NAMESPACE is terminating"
 }
 EOF
 )
